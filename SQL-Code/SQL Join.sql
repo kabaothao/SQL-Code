d32f7bfc55b5eb4cfa7d3e1aa595a4cf6c0c8627
Kabao Thao <KabaoKThao@gillettechildrens.com>
2022-02-02 15:42:54 -0600

    add join file

---
+++ SQL-Code/SQL Join.sql
@@ -0,0 +1,41 @@
+﻿/*
+Today's Topic: Inner Joins, Full/Left/Right Outer Joins
+*/
+
+SELECT *
+FROM mytestdb.dbo.EmployeeDemographics
+
+SELECT *
+FROM mytestdb.dbo.EmployeeSalary
+
+
+SELECT * 
+FROM mytestdb.dbo.EmployeeDemographics
+Inner Join mytestdb.dbo.EmployeeSalary
+	ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
+
+SELECT * 
+FROM mytestdb.dbo.EmployeeDemographics
+Full Outer Join mytestdb.dbo.EmployeeSalary
+	ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
+
+SELECT * 
+FROM mytestdb.dbo.EmployeeDemographics
+Left Outer Join mytestdb.dbo.EmployeeSalary
+	ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
+
+SELECT * 
+FROM mytestdb.dbo.EmployeeDemographics
+Right Outer Join mytestdb.dbo.EmployeeSalary
+	ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
+
+SELECT EmployeeSalary.EmployeeID, FirstName, LastName, JobTitle, Salary
+FROM mytestdb.dbo.EmployeeDemographics
+Left Outer Join mytestdb.dbo.EmployeeSalary
+	ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
+
+	
+SELECT EmployeeSalary.EmployeeID, FirstName, LastName, JobTitle, Salary
+FROM mytestdb.dbo.EmployeeDemographics
+Left Outer Join mytestdb.dbo.EmployeeSalary
+	ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
